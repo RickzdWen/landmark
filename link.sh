f806059ec20da2942b4b199397d0d621ae3855fe
RickzdWen <zhengdwen@qq.com>
2015-01-18 15:25:27 +0800

    link

--- link.sh
+++ link.sh
@@ -3,7 +3,7 @@ ln -s ../landmark_oa/configs ./configs
 ln -s ../landmark_oa/libs ./libs
 ln -s ../landmark_oa/services ./services
 cd ./public/images/
-ln -s ../landmark_oa/public/images/home_banners ./home_banners
-ln -s ../landmark_oa/public/images/products ./products
-ln -s ../landmark_oa/public/images/special_offers ./special_offers
-ln -s ../landmark_oa/public/images/ueditor ./ueditor
+ln -s ../../../landmark_oa/public/images/home_banners ./home_banners
+ln -s ../../../landmark_oa/public/images/products ./products
+ln -s ../../../landmark_oa/public/images/special_offers ./special_offers
+ln -s ../../../landmark_oa/public/images/ueditor ./ueditor
