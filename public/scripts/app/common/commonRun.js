298abf3ed4280fa158f1c80278c26b40c99359ff
RickzdWen <zhengdwen@qq.com>
2015-02-11 02:25:58 +0800

    add to cart animate

--- public/scripts/app/common/commonRun.js
+++ public/scripts/app/common/commonRun.js
@@ -25,15 +25,31 @@ define([
         return o;
     };
 
+    $('body').after('<i class="fa fa-shopping-cart" id="cartIcon" style="position: absolute;display: none;"></i>');
+    var $cartIcon = $('#cartIcon');
     $('#wrapper').on('click', '.js-add-to-cart', function(e){
         e.preventDefault();
+        $cartIcon.show();
+//        $cartIcon.offset($('#cart').offset());
+//        var top = $cartIcon.css('top');
+//        var left = $cartIcon.css('left');
+        $cartIcon.offset($(this).offset()).show();
         cart.addToCart({
             sid : $(this).data('sid'),
             pid : $(this).data('pid') || '',
             qty : $(this).data('qty')
         }).then(function(ret){
-            topic.publish('getCartList');
-            console.log(ret);
+            var target = $('#cart').offset();
+            $cartIcon.animate({
+                top : target.top + 'px',
+                left : target.left + 'px'
+            }, {
+                duration : 'normal',
+                always : function(){
+                    $('#cartIcon').hide();
+                    topic.publish('getCartList');
+                }
+            });
         }, function(error){
             if (error && error.code == 50000) {
                 window.location.href = '/login?ref=' + encodeURIComponent(window.location.href);
