a51ad36cb5879852d4ccfbe410b21932462a7320
RickzdWen <zhengdwen@qq.com>
2015-03-31 18:36:33 +0800

    address

---
+++ public/scripts/app/account/AddressForm.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Windows7 on 2015/3/31.
+ */
+
+define([
+    'jquery',
+    'landmark/declare',
+    './CheckoutForm'
+], function($, declare, CheckoutForm){
+    return declare([CheckoutForm], {
+        
+    });
+});
