bb03b50b0e768f97fee0651da031bc2240cbe3b7
RickzdWen <zhengdwen@qq.com>
2015-05-19 00:28:52 +0800

    order list

---
+++ public/scripts/app/account/ordersMain.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2015/5/19.
+ */
+
+require([
+    'domReady!',
+    'jquery',
+    'app/common/commonRun',
+    'app/common/makeTableResponsive'
+], function(doc, $){
+});
