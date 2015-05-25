278ee23d008557b99f4ccb3ff2144e874a9cb2f4
RickzdWen <zhengdwen@qq.com>
2015-05-26 00:07:46 +0800

    order detail

--- controllers/account/order.js
+++ controllers/account/order.js
@@ -3,11 +3,22 @@
  */
 
 var CommonError = require(ROOT_PATH + '/libs/errors/CommonError');
+var OrderService = require(ROOT_PATH + '/services/OrderService');
 
 module.exports = function(router) {
     router.get('/:id', function(req, res, next){
         try {
-            
+            OrderService.getOrderDetail(req.params.id, req.session.uid).then(function(data){
+                var util = require(ROOT_PATH + '/libs/util');
+                if (data) {
+                    data.id = req.params.id;
+                    data.countries = util.countries;
+                    data.usStates = util.usStates;
+                }
+                res.render('account/order', data);
+            }, function(err){
+                next(err);
+            })
         } catch (err) {
             next(err);
         }
